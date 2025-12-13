dd7b55011b3617929fc1c58109d2e29bf75a84fb
PL <109916837+PankajLohaniOfficial@users.noreply.github.com>
2025-12-13 08:11:41 +0530

    Added example of increment and decrement operators
    Demonstrate increment and decrement operators in Java.

---
+++ d_increment_and_decrement_operator.java
@@ -0,0 +1,7 @@
+public class d_increment_and_decrement_operator {
+    public static void main ( String [] args){
+        int i = 56;
+        System.out.println(i++); // The output of this will be - > 56 since the value is first used then it is incremented..
+        System.out.println(++i); // The output of this will be - > 58 since in previously the 56 was increased to 57 and then now it is first updated then it is use ....
+    }
+}
